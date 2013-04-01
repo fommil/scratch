e004ccb8fa558f2c44aa98c09aa1415956f42407
Sam Halliday <sam.halliday@gmail.com>
2013-04-01 16:01:32 +0100

    imported answer from previous course

--- funsets/src/test/scala/funsets/FunSetSuite.scala
+++ funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -18,7 +18,7 @@ class FunSetSuite extends FunSuite {
   /**
    * Link to the scaladoc - very clear and detailed tutorial of FunSuite
    *
-   * http://doc.scalatest.org/1.9.1/index.html#org.scalatest.FunSuite
+   * http://doc.scalatest.org/1.8/index.html#org.scalatest.FunSuite
    *
    * Operators
    *  - test
@@ -86,7 +86,7 @@ class FunSetSuite extends FunSuite {
    * Once you finish your implementation of "singletonSet", exchange the
    * function "ignore" by "test".
    */
-  ignore("singletonSet(1) contains 1") {
+  test("singletonSet(1) contains 1") {
     
     /**
      * We create a new instance of the "TestSets" trait, this gives us access
@@ -101,12 +101,105 @@ class FunSetSuite extends FunSuite {
     }
   }
 
-  ignore("union contains all elements") {
+  test("union contains all elements") {
     new TestSets {
       val s = union(s1, s2)
       assert(contains(s, 1), "Union 1")
       assert(contains(s, 2), "Union 2")
       assert(!contains(s, 3), "Union 3")
     }
+  }
+
+  test("intersect ...") {
+    new TestSets {
+      assert(!contains(intersect(s1, s2), 1), "Intersect 1")
+      assert(!contains(intersect(s1, s2), 2), "Intersect 2")
+      assert(!contains(intersect(s1, s2), 3), "Intersect 3")
+      assert(contains(intersect(s1, s1), 1), "Intersect 4")
+    }
+  }
+
+  test("diff ...") {
+    new TestSets {
+      assert(!contains(diff(s1, s1), 1), "Diff 1")
+      assert(!contains(diff(x => true, s1), 1), "Diff 2")
+      assert(contains(diff(x => true, s2), 1), "Diff 3")
+    }
+  }
+
+  test("filter ...") {
+    new TestSets {
+      assert(contains(filter(s1, s1), 1), "Filter 1")
+      assert(!contains(filter(s1, s2), 1), "Filter 2")
+      assert(contains(filter(x => true, s1), 1), "Filter 3")
+      assert(!contains(filter(x => true, s1), 2), "Filter 3a")
+      assert(contains(filter(s1, x => true), 1), "Filter 4")
+      assert(!contains(filter(x => false, s1), 1), "Filter 5")
+      assert(!contains(filter(s1, x => false), 1), "Filter 6")
+    }
+  }
+
+  object TestFunctions {
+    def dirac(i : Int) = (j: Int) => i == j
+
+    val odd = (i: Int) => (i & 1) == 1
+
+    val even = (i: Int) => (i & 1) != 1
+
+    val trues = (_ : Int) => true
+
+    val falses = (_ : Int) => false
+
+    val isPrime = (i : Int) => {
+      if (i <= 1) false
+      else if (i == 2) true
+      else !(2 until i).exists(x => (i % x) == 0)
+    }
+
+    val mine = (i : Int) => odd(i) && isPrime(i)
+
+    def add(i: Int) = (j: Int) => i + j
+
+    def mult(i: Int) = (j: Int) => i * j
+  }
+
+  test("forall") {
+    import TestFunctions._
+
+    assert(forall(dirac(0), dirac(0)))
+    assert(! (forall(dirac(1), dirac(0))))
+    assert(! (forall(odd, dirac(0))))
+    assert(forall(dirac(1), odd))
+    assert(forall(mine, odd))
+    assert(!forall(trues, falses))
+    assert(forall(trues, trues))
+    assert(forall(falses, falses))
+    assert(forall(falses, trues))
+  }
+
+  test("exists") {
+    import TestFunctions._
+
+    assert(exists(dirac(0), dirac(0)))
+    assert(! (exists(dirac(1), dirac(0))))
+    assert(! (exists(odd, dirac(0))))
+    assert(exists(dirac(1), odd))
+    assert(exists(mine, odd))
+    assert(!exists(trues, falses))
+    assert(exists(trues, trues))
+    assert(!exists(falses, falses))
+    assert(!exists(falses, trues))
+  }
+
+  test("map") {
+    import TestFunctions._
+
+    assert(forall(map(dirac(0), add(1)), dirac(1)))
+
+    assert(forall(map(dirac(0), mult(10)), dirac(0)))
+    assert(forall(map(dirac(1), mult(10)), dirac(10)))
+
+    val mult2 = map(trues, mult(2))
+    assert(forall(mult2, even), printSet(mult2))
   }
 }
