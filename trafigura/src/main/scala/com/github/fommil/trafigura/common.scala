0177165d45af7845791e5e674017de87c3ac90ec
Sam Halliday <sam.halliday@gmail.com>
2013-10-20 17:50:15 +0100

    implemented movement of pieces

--- trafigura/src/main/scala/com/github/fommil/trafigura/common.scala
+++ trafigura/src/main/scala/com/github/fommil/trafigura/common.scala
@@ -2,6 +2,10 @@ package com.github.fommil.trafigura
 
 import akka.contrib.jul.JavaLogging
 
+object `package` {
+  // zero-indexed (x, y)
+  type Position = (Int, Int)
+}
 
 /** Find all unique configurations of chess pieces on a board of dimension `M × N`,
   * where none of the pieces can capture any of the others, ignoring colour.
@@ -10,21 +14,83 @@ import akka.contrib.jul.JavaLogging
   */
 trait ChessSolver
 
-sealed trait Piece
+case class GameState(board: Board, pieces: Map[Position, Piece] = Map()) {
+  def withPiece(pos: Position, piece: Piece) = {
+    require(board.isValid(pos))
+    copy(pieces = pieces + (pos -> piece))
+  }
+}
 
-case class King() extends Piece
 
-case class Queen() extends Piece
+case class Board(width: Int, height: Int) {
+  require(width > 0 && height > 0)
 
-case class Rook() extends Piece
+  def isValid(p: Position) = {
+    p._1 >= 0 && p._2 >= 0 && p._1 < width && p._2 < height
+  }
+}
 
-case class Bishop() extends Piece
+sealed trait Piece {
+  def canTake(from: Position, board: Board)(to: Position) = moves(from, board).contains(to)
 
-case class Horsey() extends Piece
+  def moves(from: Position, board: Board): Seq[Position]
+
+  // all valid diagonal moves to a range.
+  // could be optimised (code complexity)
+  // to avoid the filter: benefits negligible
+  protected def diag(p: Position, range: Int, board: Board) = {
+    assert(range > 0)
+    for (x <- 1 to range) yield
+      (p._1 - x, p._2 - x) ::
+        (p._1 + x, p._2 - x) ::
+        (p._1 - x, p._2 + x) ::
+        (p._1 + x, p._2 + x) :: Nil
+  }.flatten filter board.isValid
+
+  protected def xy(p: Position, range: Int, board: Board) = {
+    assert(range > 0)
+    for (x <- 1 to range) yield
+      (p._1, p._2 - x) ::
+        (p._1, p._2 + x) ::
+        (p._1 - x, p._2) ::
+        (p._1 + x, p._2) :: Nil
+  }.flatten filter board.isValid
+
+  // largest distance to any side of the board
+  protected def toSide(p: Position, board: Board) =
+    (p._1 :: board.width - p._1 :: p._2 :: board.height - p._2 :: Nil).max
+
+}
+
+case class King() extends Piece {
+  def moves(p: Position, board: Board) = xy(p, 1, board) ++ diag(p, 1, board)
+}
+
+case class Queen() extends Piece {
+  def moves(p: Position, board: Board) = {
+    val range = toSide(p, board)
+    xy(p, range, board) ++ diag(p, range, board)
+  }
+}
+
+case class Rook() extends Piece {
+  def moves(p: Position, board: Board) = xy(p, toSide(p, board), board)
+}
+
+case class Bishop() extends Piece {
+  def moves(p: Position, board: Board) = diag(p, toSide(p, board), board)
+}
+
+case class Horsey() extends Piece {
+  def moves(p: Position, board: Board) = {
+    (p._1 + 2, p._2 + 1) ::(p._1 + 2, p._2 - 1) ::
+      (p._1 - 2, p._2 + 1) ::(p._1 - 2, p._2 - 1) ::
+      (p._1 - 1, p._2 + 2) ::(p._1 + 1, p._2 + 2) ::
+      (p._1 - 1, p._2 - 2) ::(p._1 + 1, p._2 - 2) :: Nil
+  } filter board.isValid
+}
+
 
 object Chess extends App with JavaLogging {
   log.info("hello world")
 }
-
-
-
---
+++ trafigura/src/test/scala/com/github/fommil/trafigura/PiecesSpecs.scala
@@ -0,0 +1,97 @@
+package com.github.fommil.trafigura
+
+import org.specs2.execute.Success
+
+/** @author Sam Halliday */
+class PiecesSpecs extends Specs {
+
+  "king" should {
+    "have three moves from a corner" in {
+      King().moves((0, 0), Board(9, 9)) must containTheSameElementsAs {
+        (0, 1) ::(1, 0) ::(1, 1) :: Nil
+      }
+    }
+
+    "have eight moves from the middle" in {
+      King().moves((4, 4), Board(9, 9)) must containTheSameElementsAs {
+        (3, 3) ::(3, 4) ::(4, 3) ::(3, 5) ::(5, 3) ::(4, 5) ::(5, 4) ::(5, 5) :: Nil
+      }
+    }
+  }
+
+  "bishop" should {
+    "move one direction from a corner" in {
+      Bishop().moves((0, 0), Board(9, 9)) must containTheSameElementsAs {
+        (1, 1) ::(2, 2) ::(3, 3) ::(4, 4) ::(5, 5) ::(6, 6) ::(7, 7) ::(8, 8) :: Nil
+      }
+    }
+
+    "move four directions from the middle" in {
+      Bishop().moves((4, 4), Board(9, 9)) must containTheSameElementsAs {
+        (0, 0) ::(1, 1) ::(2, 2) ::(3, 3) ::(5, 5) ::(6, 6) ::(7, 7) ::(8, 8) ::
+          (3, 5) ::(2, 6) ::(1, 7) ::(0, 8) ::
+          (5, 3) ::(6, 2) ::(7, 1) ::(8, 0) :: Nil
+      }
+    }
+  }
+
+  "rook" should {
+    "move two directions from a corner" in {
+      Rook().moves((0, 0), Board(9, 9)) must containTheSameElementsAs {
+        (0, 1) ::(0, 2) ::(0, 3) ::(0, 4) ::(0, 5) ::(0, 6) ::(0, 7) ::(0, 8) ::
+          (1, 0) ::(2, 0) ::(3, 0) ::(4, 0) ::(5, 0) ::(6, 0) ::(7, 0) ::(8, 0) :: Nil
+      }
+    }
+
+    "move four directions from the middle" in {
+      Rook().moves((4, 4), Board(9, 9)) must containTheSameElementsAs {
+        (4, 0) ::(4, 1) ::(4, 2) ::(4, 3) ::(4, 5) ::(4, 6) ::(4, 7) ::(4, 8) ::
+          (0, 4) ::(1, 4) ::(2, 4) ::(3, 4) ::(5, 4) ::(6, 4) ::(7, 4) ::(8, 4) :: Nil
+      }
+    }
+  }
+
+  "queen" should {
+    "move three directions from a corner" in {
+      Queen().moves((0, 0), Board(9, 9)) must containTheSameElementsAs {
+        (0, 1) ::(0, 2) ::(0, 3) ::(0, 4) ::(0, 5) ::(0, 6) ::(0, 7) ::(0, 8) ::
+          (1, 0) ::(2, 0) ::(3, 0) ::(4, 0) ::(5, 0) ::(6, 0) ::(7, 0) ::(8, 0) ::
+          (1, 1) ::(2, 2) ::(3, 3) ::(4, 4) ::(5, 5) ::(6, 6) ::(7, 7) ::(8, 8) :: Nil
+      }
+    }
+
+    "move eight directions from the middle" in {
+      Queen().moves((4, 4), Board(9, 9)) must containTheSameElementsAs {
+        (0, 0) ::(1, 1) ::(2, 2) ::(3, 3) ::(5, 5) ::(6, 6) ::(7, 7) ::(8, 8) ::
+          (3, 5) ::(2, 6) ::(1, 7) ::(0, 8) ::
+          (5, 3) ::(6, 2) ::(7, 1) ::(8, 0) ::
+          (4, 0) ::(4, 1) ::(4, 2) ::(4, 3) ::(4, 5) ::(4, 6) ::(4, 7) ::(4, 8) ::
+          (0, 4) ::(1, 4) ::(2, 4) ::(3, 4) ::(5, 4) ::(6, 4) ::(7, 4) ::(8, 4) :: Nil
+      }
+    }
+  }
+
+  "horsey" should {
+    "have two moves from a corner" in {
+      Horsey().moves((0, 0), Board(9, 9)) must containTheSameElementsAs {
+        (2, 1) ::(1, 2) :: Nil
+      }
+    }
+
+    "have 8 moves from the centre" in {
+      Horsey().moves((4, 4), Board(9, 9)) must containTheSameElementsAs {
+        (6, 3) ::(6, 5) ::
+          (2, 3) ::(2, 5) ::
+          (3, 6) ::(5, 6) ::
+          (3, 2) ::(5, 2) :: Nil
+      }
+    }
+  }
+
+  "wookie" should {
+    "always win" in {
+      Success("or he'll rip your arms off")
+    }
+  }
+
+}
