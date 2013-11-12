e936f0b4559573dc1582f835d091f29d8be55fcf
Sam Halliday <sam.halliday@gmail.com>
2013-11-12 22:38:25 +0000

    implement epidemy

--- simulations/src/main/scala/simulations/EpidemySimulator.scala
+++ simulations/src/main/scala/simulations/EpidemySimulator.scala
@@ -11,25 +11,88 @@ class EpidemySimulator extends Simulator {
     val roomRows: Int = 8
     val roomColumns: Int = 8
 
-    // to complete: additional parameters of simulation
+    val transmissibility = 0.4
+    val prevalence = 0.01
+    val deathRate = 0.25
   }
 
   import SimConfig._
 
-  val persons: List[Person] = List() // to complete: construct list of persons
+  val persons: List[Person] = (0 until population).map {
+    new Person(_)
+  }.toList
 
-  class Person (val id: Int) {
-    var infected = false
+  def inRoom(r: (Int, Int)) = persons.filter {
+    p =>
+      p.row == r._1 && p.col == r._2
+  }
+
+  class Person(val id: Int) {
+    var infected = id < population * prevalence
     var sick = false
     var immune = false
     var dead = false
-
-    // demonstrates random number generation
     var row: Int = randomBelow(roomRows)
     var col: Int = randomBelow(roomColumns)
 
-    //
-    // to complete with simulation logic
-    //
+    afterDelay(0) {
+      mode
+    }
+
+    private def mode {
+      if (dead) return
+      val wait = randomBelow(5)
+      val choice = chooseRoom
+      afterDelay(wait) {
+        choice match {
+          case None => mode
+          case Some(room) => move(room)
+        }
+      }
+    }
+
+    private def move(room: (Int, Int)) {
+      if (dead) return
+      row = room._1
+      col = room._2
+      if (inRoom(room).count(_.infected) > 0 && random < transmissibility) {
+        infected = true
+        afterDelay(6) {
+          sick = true
+          afterDelay(8) {
+            if (random < deathRate)
+              dead = true
+            else afterDelay(2) {
+              immune = true
+              afterDelay(2) {
+                infected = false
+                sick = false
+                immune = false
+              }
+            }
+          }
+        }
+      }
+
+      afterDelay(1) {
+        mode
+      }
+    }
+
+    private def chooseRoom = {
+      val ns = neighbours.toList
+      if (ns.isEmpty) None
+      else Some(ns(randomBelow(ns.size)))
+    }
+
+    private def neighbours = for {
+      move <- (-1, 0) ::(1, 0) ::(0, -1) ::(0, 1) :: Nil
+      room = (
+        (roomRows + move._1 + row) % roomRows,
+        (roomColumns + move._2 + col) % roomColumns
+        )
+      if inRoom(room).count(_.sick) == 0
+    } yield room
   }
+
 }
