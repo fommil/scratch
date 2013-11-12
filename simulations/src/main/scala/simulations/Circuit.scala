2cd95d8eee7b7bf830a2ea601a51c53a7f255be2
Sam Halliday <sam.halliday@gmail.com>
2013-11-12 21:09:05 +0000

    implement and, or and demux

--- simulations/src/main/scala/simulations/Circuit.scala
+++ simulations/src/main/scala/simulations/Circuit.scala
@@ -1,6 +1,7 @@
 package simulations
 
 import common._
+import math.pow
 
 class Wire {
   private var sigVal = false
@@ -48,26 +49,48 @@ abstract class CircuitSimulator extends Simulator {
     def andAction() {
       val a1Sig = a1.getSignal
       val a2Sig = a2.getSignal
-      afterDelay(AndGateDelay) { output.setSignal(a1Sig & a2Sig) }
+      afterDelay(AndGateDelay) { output.setSignal(a1Sig && a2Sig) }
     }
     a1 addAction andAction
     a2 addAction andAction
   }
 
-  //
-  // to complete with orGates and demux...
-  //
-
   def orGate(a1: Wire, a2: Wire, output: Wire) {
-    ???
+    def orAction() {
+      val a1Sig = a1.getSignal
+      val a2Sig = a2.getSignal
+      afterDelay(OrGateDelay) { output.setSignal(a1Sig || a2Sig) }
+    }
+    a1 addAction orAction
+    a2 addAction orAction
   }
   
   def orGate2(a1: Wire, a2: Wire, output: Wire) {
-    ???
+    // x OR y = NOT((NOT x) AND (NOT y))
+    val a1i, a2i, a3 = new Wire
+    inverter(a1, a1i)
+    inverter(a2, a2i)
+    andGate(a1i, a2i, a3)
+    inverter(a3, output)
   }
 
   def demux(in: Wire, c: List[Wire], out: List[Wire]) {
-    ???
+    assume((BigInt(2) pow c.size) == out.size)
+    c match {
+      case Nil =>
+        orGate(new Wire, in, out.head)
+
+      case head :: tail =>
+        val split = out.splitAt(out.size / 2)
+        val left, alt, right = new Wire
+        andGate(in, head, left)
+
+        inverter(left, alt)
+        andGate(in, alt, right)
+
+        demux(left, tail, split._1)
+        demux(right, tail, split._2)
+    }
   }
 
 }
--- simulations/src/test/scala/simulations/CircuitSuite.scala
+++ simulations/src/test/scala/simulations/CircuitSuite.scala
@@ -31,8 +31,107 @@ class CircuitSuite extends CircuitSimulator with FunSuite {
     assert(out.getSignal === true, "and 3")
   }
 
-  //
-  // to complete with tests for orGate, demux, ...
-  //
+  test("orGate example") {
+    val in1, in2, out = new Wire
+    orGate(in1, in2, out)
+    in1.setSignal(false)
+    in2.setSignal(false)
+    run
+
+    assert(out.getSignal === false, "or 1")
+
+    in1.setSignal(true)
+    run
+
+    assert(out.getSignal === true, "or 2")
+
+    in2.setSignal(true)
+    run
+
+    assert(out.getSignal === true, "or 3")
+
+    in1.setSignal(false)
+    run
+
+    assert(out.getSignal === true, "or 4")
+  }
+
+  test("orGate2 example") {
+    val in1, in2, out = new Wire
+    orGate2(in1, in2, out)
+    in1.setSignal(false)
+    in2.setSignal(false)
+    run
+
+    assert(out.getSignal === false, "or 1")
+
+    in1.setSignal(true)
+    run
+
+    assert(out.getSignal === true, "or 2")
+
+    in2.setSignal(true)
+    run
+
+    assert(out.getSignal === true, "or 3")
+
+    in1.setSignal(false)
+    run
+
+    assert(out.getSignal === true, "or 4")
+  }
+
+  test("demux small") {
+    val in, out = new Wire
+    demux(in, Nil, out :: Nil)
+
+    run
+    assert(out.getSignal === false)
+
+    in.setSignal(true)
+    run
+    assert(out.getSignal === true)
+  }
+
+  test("demux medium") {
+    val in, c0, c1, o0, o1, o2, o3 = new Wire
+    val c = c1 :: c0 :: Nil
+    val o = o3 :: o2 :: o1 :: o0 :: Nil
+    demux(in, c, o)
+
+    run
+    assert(o3.getSignal === false, 0)
+    assert(o2.getSignal === false, 1)
+    assert(o1.getSignal === false, 2)
+    assert(o0.getSignal === false, 3)
+
+    in.setSignal(true)
+    run
+    assert(o3.getSignal === false, 0)
+    assert(o2.getSignal === false, 1)
+    assert(o1.getSignal === false, 2)
+    assert(o0.getSignal === true, 3)
+
+    c0.setSignal(true)
+    run
+    assert(o3.getSignal === false, 0)
+    assert(o2.getSignal === false, 1)
+    assert(o1.getSignal === true, 2)
+    assert(o0.getSignal === false, 3)
+
+    c1.setSignal(true)
+    run
+    assert(o3.getSignal === true, 0)
+    assert(o2.getSignal === false, 1)
+    assert(o1.getSignal === false, 2)
+    assert(o0.getSignal === false, 3)
+
+    c0.setSignal(false)
+    run
+    assert(o3.getSignal === false, 0)
+    assert(o2.getSignal === true, 1)
+    assert(o1.getSignal === false, 2)
+    assert(o0.getSignal === false, 3)
+  }
 
 }
