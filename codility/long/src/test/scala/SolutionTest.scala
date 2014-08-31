2609dcd360163482893ad5419bbce81cfb3a9215
Sam Halliday <sam.halliday@gmail.com>
2014-08-31 21:19:19 +0100

    more testing on second test

--- codility/long/src/test/scala/SolutionTest.scala
+++ codility/long/src/test/scala/SolutionTest.scala
@@ -3,18 +3,36 @@ import org.scalatest.matchers.ShouldMatchers
 import Solution.solution
 
 class SolutionTest extends FunSpec with ShouldMatchers {
+
+  val ex1 = Array(10, 2, 5, 1, 8, 20)
+  val ex2 = Array(5, 10, 18, 7, 8, 3)
+  val ex3 = Array(10, 20, 30)
+
   describe("Solution") {
     it("should pass the first example") {
-      val res = solution(Array(10, 2, 5, 1, 8, 20))
-      assert(res === 23)
+      assert(solution(ex1) === 23)
     }
     it("should pass the second example") {
-      val res = solution(Array(5, 10, 18, 7, 8, 3))
-      assert(res === 15)
+      assert(solution(ex2) === 15)
     }
     it("should pass the third example") {
-      val res = solution(Array(10, 20, 30))
-      assert(res === -1)
+      assert(solution(ex3) === -1)
+    }
+
+    it("should pass variations of the first example") {
+      ex1.permutations foreach { ex =>
+        assert(solution(ex) === 23)
+      }
+    }
+    it("should pass variations of the second example") {
+      ex2.permutations foreach { ex =>
+        assert(solution(ex) === 15)
+      }
+    }
+    it("should pass variations of the third example") {
+      ex3.permutations foreach { ex =>
+        assert(solution(ex) === -1)
+      }
     }
   }
 }
